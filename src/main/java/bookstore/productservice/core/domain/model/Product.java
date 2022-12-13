a0fd1e97194556e66042ba4124fee1ee21e79c6c
younes <younes.abdelwadoud@outlook.de>
2022-12-13 16:34:20 +0100

    [KBE-5] Updated product class

--- src/main/java/bookstore/productservice/core/domain/model/Product.java
+++ src/main/java/bookstore/productservice/core/domain/model/Product.java
@@ -1,23 +1,45 @@
 package bookstore.productservice.core.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 import java.util.UUID;
 
 public class Product {
 
-    private UUID id;
-    private String isbn10;
-    private String isbn13;
-    private String title;
-    private String version;
-    private String[] authors;
-    private Date publishingDate;
-    private String publishingHouse;
-    private String description;
-    private String language;
-    private int pages;
-    private String coverUrl;
-    private float price;
+    @Getter private UUID id;
+    @Getter private final String isbn10;
+    @Getter private final String isbn13;
+    @Getter private final String title;
+    @Getter private final String version;
+    @Getter private final String[] authors;
+    @Getter private final Date publishingDate;
+    @Getter private final String publishingHouse;
+    @Getter private final String description;
+    @Getter private final String language;
+    @Getter private final int pages;
+    //@Getter private String coverUrl;
+    @Getter @Setter private float price;
+
+
+    public Product(String isbn10, String isbn13, String title, String version, String[] authors, Date publishingDate,
+                   String publishingHouse, String description, String language, int pages, float price) {
+        //TODO: UUID auto gen
+        this.isbn10 = isbn10;
+        this.isbn13 = isbn13;
+        this.title = title;
+        this.version = version;
+        this.authors = authors;
+        this.publishingDate = publishingDate;
+        this.publishingHouse = publishingHouse;
+        this.description = description;
+        this.language = language;
+        this.pages = pages;
+        this.price = price;
+    }
+
+
 
 }
 
