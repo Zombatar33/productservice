2c031045e450b775a2e0621277b701bbf30a0813
Lechiffre2110 <73186756+Lechiffre2110@users.noreply.github.com>
2022-12-23 15:25:24 +0100

    [KBE-5] refactor: add schema validation

--- src/main/java/bookstore/productservice/core/domain/model/Product.java
+++ src/main/java/bookstore/productservice/core/domain/model/Product.java
@@ -1,44 +1,56 @@
 package bookstore.productservice.core.domain.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 import java.util.UUID;
 
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
     @Getter private UUID id;
-    @Getter private final String isbn10;
-    @Getter private final String isbn13;
-    @Getter private final String title;
-    @Getter private final String version;
-    @Getter private final String[] authors;
-    @Getter private final Date publishingDate;
-    @Getter private final String publishingHouse;
-    @Getter private final String description;
-    @Getter private final String language;
-    @Getter private final int pages;
-    //@Getter private String coverUrl;
+
+    @Column(nullable = false)
+    @Getter private String isbn13;
+
+    @Column(nullable = false)
+    @Getter private String title;
+
+    @Column(nullable = false)
+    @Getter private String version;
+
+    @Column(nullable = false)
+    @Getter private String[] authors;
+
+    @Column(nullable = false)
+    @Getter private Date publishingDate;
+
+    @Column(nullable = false)
+    @Getter private String publishingHouse;
+
+    @Column(nullable = false)
+    @Getter private String description;
+
+    @Column(nullable = false)
+    @Getter private String language;
+
+    @Column(nullable = false)
+    @Getter private int pages;
+
+    @Column(nullable = true)
+    @Getter private String coverUrl;
+
+    @Column(nullable = false)
     @Getter @Setter private float price;
-
-
-    public Product(String isbn10, String isbn13, String title, String version, String[] authors, Date publishingDate,
-                   String publishingHouse, String description, String language, int pages, float price) {
-        //TODO: UUID auto gen
-        this.isbn10 = isbn10;
-        this.isbn13 = isbn13;
-        this.title = title;
-        this.version = version;
-        this.authors = authors;
-        this.publishingDate = publishingDate;
-        this.publishingHouse = publishingHouse;
-        this.description = description;
-        this.language = language;
-        this.pages = pages;
-        this.price = price;
-    }
-
 
 
 }
