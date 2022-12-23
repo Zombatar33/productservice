6571dfaa7c7d93d86b1f65667d5c577b8252ac3f
Lechiffre2110 <73186756+Lechiffre2110@users.noreply.github.com>
2022-12-23 15:36:33 +0100

    [KBE-11] feature: setup PostgresDB connection and dummy data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO products VALUES
+	('978-3-86640-001-6', 'buch1', '1.0', '{Ian Flemming}', '2022-04-04', 'VerlagA', 'Ein Buch A', 'deutsch', '123', '20');
+	('978-3-86640-001-7', 'buch2', '1.0', '{Ian Flemming}', '2022-04-04', 'VerlagA', 'Ein Buch B', 'deutsch', '123', '25');
+	('978-3-86640-001-8', 'buch3', '1.0', '{Ian Flemming}', '2022-04-04', 'VerlagC', 'Ein Buch C', 'deutsch', '123', '30');
+    ('978-3-86640-001-9', 'buch4', '1.0', '{Ian Flemming}', '2022-04-04', 'VerlagB', 'Ein Buch D', 'deutsch', '123', '20');
