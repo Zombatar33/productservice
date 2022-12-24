1928c82aa2c8f1ad8c053ad58b166c7d325bfc59
Lechiffre2110 <73186756+Lechiffre2110@users.noreply.github.com>
2022-12-24 21:59:07 +0100

    [KBE-15] feature: add docker setup

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO products (id, isbn13, title, version, authors, publishing_date, publishing_house, description, language, pages, price)
-VALUES ('033c50c8-823b-11ed-a1eb-0242ac120002', '978-3-86640-001-6', 'buch1', '1.0', '{Ian Flemming}', '2022-04-04', 'VerlagA', 'Ein Buch', 'deutsch', '123', '20');
-
-INSERT INTO products (id, isbn13, title, version, authors, publishing_date, publishing_house, description, language, pages, price)
-VALUES ('033c5384-823b-11ed-a1eb-0242ac120002','978-3-86640-001-7', 'buch2', '1.0', '{Ian Flemming}', '2022-04-04', 'VerlagA', 'Ein Buch', 'deutsch', '123', '25');
-
-INSERT INTO products (id, isbn13, title, version, authors, publishing_date, publishing_house, description, language, pages, price)
-VALUES ('033c54c4-823b-11ed-a1eb-0242ac120002', '978-3-86640-001-8', 'buch3', '1.0', '{Ian Flemming}', '2022-04-04', 'VerlagA', 'Ein Buch', 'deutsch', '123', '30');
