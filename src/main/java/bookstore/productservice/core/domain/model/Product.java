6dc2e851f6f9fb0f05db07d2bd53f7593f1c73b4
Lechiffre2110 <73186756+Lechiffre2110@users.noreply.github.com>
2023-03-30 18:36:52 +0200

    added swagger documentation

--- src/main/java/bookstore/productservice/core/domain/model/Product.java
+++ src/main/java/bookstore/productservice/core/domain/model/Product.java
@@ -1,10 +1,7 @@
 package bookstore.productservice.core.domain.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Date;
 import java.util.UUID;
